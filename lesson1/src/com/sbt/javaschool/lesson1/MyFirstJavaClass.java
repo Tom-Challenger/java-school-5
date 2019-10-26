22c45cde9700b058f6ceece7dec23911f5b54dcf
Tom-Challenger <it.mail.art@gmail.com>
2019-10-26 13:21:45 +0300

    init commit

---
+++ lesson1/src/com/sbt/javaschool/lesson1/MyFirstJavaClass.java
@@ -0,0 +1,7 @@
+package com.sbt.javaschool.lesson1;
+
+public class MyFirstJavaClass {
+    public static void main(String[] args) {
+        System.out.println("L1");
+    }
+}
---
+++ lesson2/src/com/sbt/javaschool/lesson2/L2.java
@@ -0,0 +1,7 @@
+package com.sbt.javaschool.lesson2;
+
+public class L2 {
+    public static void main(String[] args) {
+        System.out.println("L2");
+    }
+}
